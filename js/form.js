e80f53b6ec368427e7d203c70c1cf96ff8f7ee17
parthdond@gmail.com <parthdond2006>
2021-07-05 13:14:53 +0530

    fruit catcher

--- js/form.js
+++ js/form.js
@@ -15,21 +15,13 @@ class Form{
     display() {
         this.title.html("FRUIT CATCHER");
         this.title.position(350, 50);
-        this.title.style('font-size', '70px');
-        this.title.style('color', 'skyblue');
+       
         this.input.position(550,400);
-        this.input.style('width', '200px');
-        this.input.style('height', '20px');
-        this.input.style('background', 'lavender');
+        
         this.button.position(560,500);
-        this.button.style('width', '200px');
-        this.button.style('height', '40px');
-        this.button.style('background', 'lightpink');
+        
         this.reset.position(900, 660);
-        this.reset.style('width', '100px');
-        this.reset.style('height', '30px');
-        this.reset.style('background', 'lightpink');
-
+       
         this.button.mousePressed(() => {
             this.input.hide();
             this.button.hide();
@@ -40,8 +32,8 @@ class Form{
             player.updateCount(playerCount);
             this.greeting.html("Hello " + player.name)
             this.greeting.position(400,250);
-            this.greeting.style('color', 'white');
-            this.greeting.style('font-size', '100px');
+            //this.greeting.style('color', 'white');
+            //this.greeting.style('font-size', '100px');
         });
 
         this.reset.mousePressed(() => {
